9cd1e034741f18be01fe97f0881caa0215d056a3
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-16 10:17:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Absolute_value_actvity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+
+            number1 = 6832;
+            number2 = -392;
+
+            Console.WriteLine(Math.Abs(number1));
+            Console.WriteLine(Math.Abs(number2));
+
+            Console.Read();
+                
+        }
+    }
+}
